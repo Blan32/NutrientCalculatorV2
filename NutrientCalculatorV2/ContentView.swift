ec9f3f503cb145dc47ce6cb14445b3e97493f43e
KB <kblandford@live.com>
2022-02-04 08:12:58 -0500

    Initial Commit

---
+++ NutrientCalculatorV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NutrientCalculatorV2
+//
+//  Created by Kyle Blandford on 2/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NutrientCalculatorV2/NutrientCalculatorV2App.swift
@@ -0,0 +1,17 @@
+//
+//  NutrientCalculatorV2App.swift
+//  NutrientCalculatorV2
+//
+//  Created by Kyle Blandford on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct NutrientCalculatorV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
